1d52ec6e36a395f447454912c3ded08f27d9f828
Julia-Gavrashenko <julia.gavrashenko@gmail.com>
2022-11-19 13:06:01 +0200

    fifth-commit

--- js/01-test.js
+++ js/01-test.js
@@ -1,21 +1,21 @@
-const products = [
-  { name: "Radar", price: 1300, quantity: 4 },
-  { name: "Scanner", price: 2700, quantity: 3 },
-  { name: "Droid", price: 400, quantity: 7 },
-  { name: "Grip", price: 1200, quantity: 9 },
-];
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
 
-function calculateTotalPrice(productName) {
-  // Change code below this line
+// function calculateTotalPrice(productName) {
+//   // Change code below this line
 
-  for (const product of products) {
-    if (productName === product.name) {
-      totalPrice = product.price * product.quantity;
-    }
-  }
-  return totalPrice;
-  // Change code above this line
-}
+//   for (const product of products) {
+//     if (productName === product.name) {
+//       totalPrice = product.price * product.quantity;
+//     }
+//   }
+//   return totalPrice;
+//   // Change code above this line
+// }
 
 // Тоха - приклад лодера
 // function load(functionParam, delay) {
@@ -74,7 +74,9 @@ function calculateTotalPrice(productName) {
 
 // const loaderBlock = document.getElementById('loadBlock');
 
-// load(loaderUI, 2000);
+// load(loaderUI, 2000); 
+// console.log(1, 2, 3)
+
 
 // МОДУЛЬ 4
 
@@ -146,32 +148,154 @@ function calculateTotalPrice(productName) {
 // Після оголошення об'єкта pizzaPalace ми додали колбеки і виклики методів. 
 // Будь ласка, нічого там не міняй.
 
-const pizzaPalace = {
-  pizzas: ["Ultracheese", "Smoked", "Four meats"],
-  order(pizzaName, onSuccess, onError) {
-    for (const pizza of pizzas) {
+// const pizzaPalace = {
+//   pizzas: ["Ultracheese", "Smoked", "Four meats"],
+//   order(pizzaName, onSuccess, onError) {
+//     for (const pizza of pizzas) {
 
-      if (this.pizzas.includes(pizzaName)) {
-return makePizza(pizzaName);
-      }
-    }
-  },
+//       if (this.pizzas.includes(pizzaName)) {
+// return makePizza(pizzaName);
+//       }
+//     }
+//   },
+// };
+// // Change code above this line
+
+// // Callback for onSuccess
+// function makePizza(pizzaName) {
+//   return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+// }
+
+// // Callback for onError
+// function onOrderError(error) {
+//   return `Error! ${error}`;
+// }
+
+// // Method calls with callbacks
+// pizzaPalace.order("Smoked", makePizza, onOrderError);
+// pizzaPalace.order("Four meats", makePizza, onOrderError);
+// pizzaPalace.order("Big Mike", makePizza, onOrderError);
+// pizzaPalace.order("Vienna", makePizza, onOrderError);
+
+
+
+// Потрібно написати функцію, яка приймає 1 параметр key і буде перевіряти
+// чи має об'єкт  такий ключ - поверне true, інакше false
+
+// const obj = {
+//   name: 'Jhon',
+//   car:  'Audi',
+//   carColor: 'White'
+// }
+
+
+// Напишіть функцію для перевірки об'єкта,
+// Чи є елемент простим об'єктом,
+// чи масивом, null.
+
+
+
+// Створіть функцію multiplyNumeric(obj),
+//   яка множить всі числові властивості об'єкта obj на 2.
+
+// до  виклику функції
+// let menu = {
+//   width: 200,
+//   height: 300,
+//   title: "My menu"
+// };
+//
+// multiplyNumeric(menu);
+
+// після виклику функції
+// menu = {
+//   width: 400,
+//   height: 600,
+//   title: "My menu"
+// };
+
+
+//У нас є об'єкт, в якому зберігаються зарплати
+//нашої команди
+//Напишіть код для обчислення суми всіх зарплат і
+//збережіть його результат у змінній sum.
+//Якщо об'єкт salaries порожній, то результат має бути 0
+
+const salaries = {
+  Mango: 570,
+  Poly: 900,
+  Ajax: 1470,
 };
-// Change code above this line
 
-// Callback for onSuccess
-function makePizza(pizzaName) {
-  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
-}
+function calcAllSalaries (salaries) {
+  const allNumbers = Object.values(salaries);
 
-// Callback for onError
-function onOrderError(error) {
-  return `Error! ${error}`;
-}
+// спосіб 1
+//   let result = 0;
 
-// Method calls with callbacks
-pizzaPalace.order("Smoked", makePizza, onOrderError);
-pizzaPalace.order("Four meats", makePizza, onOrderError);
-pizzaPalace.order("Big Mike", makePizza, onOrderError);
-pizzaPalace.order("Vienna", makePizza, onOrderError);
+//   for (const value of allNumbers){
+// result += value;
 
+//   }
+// return result;
+//   console.log(allNumbers);
+
+
+// спосіб 2
+// return allNumbers.reduce((sum, number) => sum + number);
+
+
+// спосіб 3
+// const salaries = {
+//   Mango: 570,
+//   Poly: 900,
+//   Ajax: 1470,
+// };
+
+// const calcSalary = obj => {
+//   let totalSalary = 0;
+//   for (const key in obj) {
+//     totalSalary += obj[key];
+//   }
+//   return totalSalary;
+// };
+
+// console.log(calcSalary(salaries));
+
+
+
+
+// };
+// console.log (calcAllSalaries(salaries));
+
+
+
+
+//Напишіть функцію calcTotalPrice(stones, stonesName),
+//яка приймає масив об'єктів і
+//рядок з назвою каменю.
+//Функція повертає загальну вартість каміння
+//з таким ім'ям
+
+// const stones = [
+//   { name: "Emerald", price: 1300, quantity: 4 },
+//   { name: "Diamond", price: 2700, quantity: 6 },
+//   { name: "Sapphire", price: 400, quantity: 7 },
+//   { name: "Rubble", price: 150, quantity: 100 },
+// ];
+
+
+
+
+// Напишіть код, щоб видалити всі елементи в заданому масиві
+// Функція `deleteElement()` видаляє всі входження елемента із заданого масиву.
+// let arr = [23,56,4,78,5,63,45,210,56];
+// arr = deleteElement(arr, 56)
+// console.log(arr); //[23, 4, 78, 5, 63, 45, 210]
+
+
+
+// Напишіть метод, який розбиває масив на частини визначеного розміру
+// const data = [1, 2, 3, 4, 5, 6, 7];
+// console.log(chunkArray(data, 2)) // [[1, 2], [3, 4], [5, 6], [7]]
+// console.log(chunkArray(data, 3)) // [[1, 2, 3], [4, 5, 6], [7]]
